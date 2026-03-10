c09078b5f00266e9dbf18c6b64a2503c0dfae235
patrickAtPwin <20310381@qq.com>
2026-03-10 21:44:29 +0800

    feat: add Tauri dialog and fs plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .manage(PythonSidecar(Mutex::new(None)))
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|_app| {

38965587a87117d73f42959c68c53fa67354d8f7
patrickAtPwin <20310381@qq.com>
2026-03-11 19:01:19 +0800

    fix: reset page loading state on app mount

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { WelcomePage, WorkspacePage, SettingsPage } from './pages';
 import { WebSocketProvider } from './contexts/WebSocketContext';
 import { LoadingOverlay } from './components/common';
+import { useUIStore } from './stores';
+import { useEffect } from 'react';
 import "./index.css";
 
 function App() {
+  const setPageLoading = useUIStore((state) => state.setPageLoading);
+
+  useEffect(() => {
+    setPageLoading(false);
+  }, [setPageLoading]);
+
   return (
     <BrowserRouter>
       <WebSocketProvider>

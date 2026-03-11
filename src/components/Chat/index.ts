5a6a40b2a558a591b99a43216c663d4889d85ac4
patrickAtPwin <20310381@qq.com>
2026-03-11 18:27:33 +0800

    feat: export status indicator components

--- src/components/Chat/index.ts
+++ src/components/Chat/index.ts
@@ -3,3 +3,5 @@ export { MessageList } from './MessageList';
 export { MessageItem } from './MessageItem';
 export { MessageInput } from './MessageInput';
 export { StreamingMessage } from './StreamingMessage';
+export { UserStatusIndicator } from './UserStatusIndicator';
+export { AssistantStatusIndicator } from './AssistantStatusIndicator';
